478a0c6e2a374cf894128dec03a7aea154e78671
ArcFreez <abhayaworks@gmail.com>
2022-11-27 16:18:44 -0700

    Adding orb logic

--- code/main.py
+++ code/main.py
@@ -73,6 +73,47 @@ def get_training_data():
     return get_images(EXP_PHRASES_DIR), get_images(MONTHS_DIR), get_images(NUMBERS_DIR)
 
 # functions to detect image
+def detect_features(gray_img):
+    detector = cv2.ORB_create(nfeatures=500,
+            nlevels=8,
+            firstLevel=0,
+            patchSize=31,
+            fastThreshold=0,
+            edgeThreshold=0)
+    keypoints, descriptors = detector.detectAndCompute(gray_img, mask=None)
+    return keypoints, descriptors
+
+def calc_homography(matches, kp_query, kp_train):
+    min_matches_for_homography = 4
+    if len(matches) < min_matches_for_homography:
+        return None, None
+        # Estimate homography transformation using RANSAC
+    src_pts = np.float32([kp_train[m.trainIdx].pt for m in matches]).reshape(
+        -1, 1, 2
+    )
+    dst_pts = np.float32([kp_query[m.queryIdx].pt for m in matches]).reshape(
+        -1, 1, 2
+    )
+    H, inliers = cv2.findHomography(
+        src_pts, dst_pts,
+        method=cv2.RANSAC
+    )
+    return H, inliers
+
+def match_features(bgr_query, bgr_train):
+    kp_train, desc_train = detect_features(bgr_train)
+    kp_query, desc_query = detect_features(bgr_query)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    matches = matcher.knnMatch(desc_query, desc_train, k=2)
+    matches = [m for m, n in matches if m.distance < 0.8 * n.distance]        
+    bgr_matches = cv2.drawMatches(
+        img1=bgr_query, keypoints1=kp_query,
+        img2=bgr_train, keypoints2=kp_train,
+        matches1to2=matches, matchesMask=None, outImg=None)
+    cv2.imshow("All matches", bgr_matches)
+    cv2.waitKey(0)
+
+
 def detect_expiry_date(image, training_imgs):
     # 1) process the image so that it is possible to detect
     # first resize the image so that it is at most 1000 x 1000
@@ -87,11 +128,12 @@ def detect_expiry_date(image, training_imgs):
     )
     # threshold black and white
     black_labels, white_labels = get_black_and_white_labels(blur)
+    _, black_labels = cv2.threshold(black_labels,0,255,cv2.THRESH_BINARY)
+    black_labels = cv2.dilate(black_labels, np.ones((1, 1), np.uint8))
     # 2) match the processed image
-    cv2.imshow('black labels', black_labels)
-    cv2.waitKey(0)
-    cv2.imshow('white labels', white_labels)
-    cv2.waitKey(0)
+    phrases, _, _ = training_imgs
+    for phrase in phrases:
+        match_features(black_labels, phrase)
 
 def main():
     images = get_test_data()
